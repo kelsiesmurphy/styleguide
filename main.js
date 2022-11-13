72fa69cf3928f3169cacee047444d15bee99503e
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-13 12:11:42 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,120 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div id="main-wrapper">
+    <section id="typography">
+      <header class="section-header">
+        <p class="section-header-text">Typography</p>
+      </header>
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">H1 Heading</p>
+        </div>
+        <div class="container-right">
+          <h1>H1 Heading</h1>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">H2 Heading</p>
+        </div>
+        <div class="container-right">
+          <h2>H2 Heading</h2>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">H3 Heading</p>
+        </div>
+        <div class="container-right">
+          <h3>H3 Heading</h3>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">H4 Heading</p>
+        </div>
+        <div class="container-right">
+          <h4>H4 Heading</h4>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">H5 Heading</p>
+        </div>
+        <div class="container-right">
+          <h5>H5 Heading</h5>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">H6 Heading</p>
+        </div>
+        <div class="container-right">
+          <h6>H6 Heading</h6>
+        </div>
+      </div>
+
+      
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">text-size-large</p>
+        </div>
+        <div class="container-right">
+          <p class ="text-size-large">
+            In a world older and more complete than ours, they move finished and complete, gifted with the extension of the senses we have lost or never attained, living by voices we shall never hear. They are not brethren, they are not underlings: they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.
+          </p>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">text-size-medium</p>
+        </div>
+        <div class="container-right">
+          <p class ="text-size-medium">
+            In a world older and more complete than ours, they move finished and complete, gifted with the extension of the senses we have lost or never attained, living by voices we shall never hear. They are not brethren, they are not underlings: they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.
+          </p>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">text-size-regular</p>
+        </div>
+        <div class="container-right">
+          <p class ="text-size-regular">
+            In a world older and more complete than ours, they move finished and complete, gifted with the extension of the senses we have lost or never attained, living by voices we shall never hear. They are not brethren, they are not underlings: they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.
+          </p>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">text-size-small</p>
+        </div>
+        <div class="container-right">
+          <p class ="text-size-small">
+            In a world older and more complete than ours, they move finished and complete, gifted with the extension of the senses we have lost or never attained, living by voices we shall never hear. They are not brethren, they are not underlings: they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.
+          </p>
+        </div>
+      </div>
+
+      <div class="container-row">
+        <div class="container-left">
+          <p class="tag">text-size-tiny</p>
+        </div>
+        <div class="container-right">
+          <p class ="text-size-tiny">
+            In a world older and more complete than ours, they move finished and complete, gifted with the extension of the senses we have lost or never attained, living by voices we shall never hear. They are not brethren, they are not underlings: they are other nations, caught with ourselves in the net of life and time, fellow prisoners of the splendour and travail of the earth.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+`
